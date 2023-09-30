e12def76046131ab9dcb64327c1fad4e8b20e321
wangmuy <wangmuy@gmail.com>
2023-09-30 12:33:06 +0800

    util getProxy from string

--- core/src/main/kotlin/com/wangmuy/llmchain/serviceprovider/Utils.kt
+++ core/src/main/kotlin/com/wangmuy/llmchain/serviceprovider/Utils.kt
@@ -1,5 +1,8 @@
 package com.wangmuy.llmchain.serviceprovider
 
+import java.net.InetSocketAddress
+import java.net.Proxy
+
 object Utils {
     fun parseProxy(proxy: String?): Triple<String?, String?, Int> {
         val splits = proxy?.split("://")
@@ -8,5 +11,14 @@ object Utils {
         val host = hostPortSplits?.get(0)
         val port = (hostPortSplits?.getOrNull(1)?.ifEmpty { "80" } ?: "80").toInt()
         return Triple(protocol, host, port)
+    }
+
+    fun getProxy(proxyStr: String?): Proxy? {
+        val (protocol, host, port) = parseProxy(proxyStr)
+        return when (protocol) {
+            "http" -> Proxy(Proxy.Type.HTTP, InetSocketAddress(host, port))
+            "socks5", "socks" -> Proxy(Proxy.Type.SOCKS, InetSocketAddress(host, port))
+            else -> null
+        }
     }
 }
